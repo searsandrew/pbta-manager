cb9440dd633d8af2c45e72a470649512ec31d2a4
Andrew Sears <searsandrew@gmail.com>
2021-08-25 08:43:21 -0500

    Testing php8 fix for undefined component variables

--- resources/views/apocalypse/create.blade.php
+++ resources/views/apocalypse/create.blade.php
@@ -11,13 +11,13 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
                 <div class="grid grid-cols-4 gap-4">
                     <div class="col-span-4 grid grid-cols-1">
-                        <x-input name="name" heading="{{ __('Name') }}" />
-                        <x-input name="stats" heading="{{ __('Stats') }}" help="{{ __('Use a comma-seperated list for each stat name') }}" />
+                        <x-input name="name" heading="{{ __('Name') }}" help="false" disabled="false" value="" />
+                        <x-input name="stats" heading="{{ __('Stats') }}" help="{{ __('Use a comma-seperated list for each stat name') }}" disabled="false" value="" />
                         <div class="grid grid-cols-4 gap-3">
-                            <x-input name="luck" heading="{{ __('Luck') }}" />
-                            <x-input name="harm" heading="{{ __('Harm') }}" />
-                            <x-input name="unstable" heading="{{ __('Unstable') }}" />
-                            <x-input name="experience" heading="{{ __('Experience') }}" />
+                            <x-input name="luck" heading="{{ __('Luck') }}" help="false" disabled="false" value="" />
+                            <x-input name="harm" heading="{{ __('Harm') }}" help="false" disabled="false" value="" />
+                            <x-input name="unstable" heading="{{ __('Unstable') }}" help="false" disabled="false" value="" />
+                            <x-input name="experience" heading="{{ __('Experience') }}" help="false" disabled="false" value="" />
                         </div>
                     </div>
                     <button type="submit" class="rounded-full py-3 px-6 mt-6 bg-green-600 col-span-4 text-green-50 flex items-center justify-center hover:bg-green-500 hover:text-green-900">{{ __('Create Apocalypse') }}</button>
