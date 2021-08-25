c589bc6c849b50fb2e66d5d1663392d22384d581
Andrew Sears <searsandrew@gmail.com>
2021-08-25 08:34:29 -0500

    Remove form element disables

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,6 +1,6 @@
 <div class="col-auto mb-3">
     <label class="block font-medium text-sm text-gray-700" }}>{{ $heading }}</label>
-    <input type="text" {{ $disabled ? 'disabled' : '' }} {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full']) }} name="{{ $name }}" value="{{ $value }}" />
+    <input type="text" {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full']) }} name="{{ $name }}" value="{{ $value }}" />
     @if($help)
         <small class="p-l3 text-gray-500 italic">{{ $help }}</small>
     @endif
--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,6 +1,6 @@
 <div class="col-auto mb-3">
     <label class="block font-medium text-sm text-gray-700" }}>{{ $label }}</label>
-    <select {{ $disabled ? 'disabled' : '' }} {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full']) }} name="{{ $name }}">
+    <select {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full']) }} name="{{ $name }}">
         @foreach($datasource as $key => $option)
             <option 
                 value="{{ $key }}"
